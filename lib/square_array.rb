bb1f7cde5317e7665bea98bf5c3cfb1baeddf14e
Charlie Payne <charliepayne1101@gmail.com>
2020-04-13 06:58:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
-def square_array(numbers)
-  new_array = []
-numbers.length.times do |index|
-  new_array.push(numbers ** 2)
-  end
-new_array
+def square_array(array)
+
+ new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
 end
