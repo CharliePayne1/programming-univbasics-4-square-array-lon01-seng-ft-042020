91c9440d3d0984355e37123b41e3c70f54dc5f7d
Charlie Payne <charliepayne1101@gmail.com>
2020-04-13 06:44:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   count = 0 
   new_array = []
   while array.length > count do
-    new_array.push(array**)
+    new_array.push(array * array)
     count += 1
   end
   new_array
